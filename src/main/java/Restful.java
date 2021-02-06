84661bf8d101b94de9f042128c708f7bcf8a2a88
Raphael Br <raphi.brunschweiler@gmail.com>
2021-02-06 09:55:31 +0100

    - Keyboard input for titles - Flushing ArrayList for new requests

--- src/main/java/Restful.java
+++ src/main/java/Restful.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -11,63 +13,81 @@ import org.json.JSONArray;
 
 public class Restful {
 
-    public static ArrayList<Movie> movies = new ArrayList<Movie>();
+    static ArrayList<Movie> movies = new ArrayList<Movie>();
 
     public static void main(String[] args) throws IOException {
 
-        // Get JSON Content
-        String url = "http://www.omdbapi.com/?apikey=58ed8f90";
-        url += "&s=" + "Tom";
+        boolean run = true;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        Response response = client.newCall(request).execute();
-
-        String responseStr = response.body().string();
-        // System.out.println(responseStr);
-
-        JSONObject jsonObj = new JSONObject(responseStr); // JSON Objekt erstellen
-        String responseServer = jsonObj.getString("Response");
-        // System.out.println("Response: " + responseServer);
-
-        if (responseServer.equals("False")) {
-            String errorRequest = jsonObj.getString("Error");
-            System.out.println("Fehler: " + errorRequest);
-        } else if (responseServer.equals("True")) {
-            JSONArray jsonArray = jsonObj.getJSONArray("Search"); // Objekt in Array
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                String title = jsonArray.getJSONObject(i).getString("Title");
-                String year = jsonArray.getJSONObject(i).getString("Year");
-                String imdb = jsonArray.getJSONObject(i).getString("imdbID");
-                String type = jsonArray.getJSONObject(i).getString("Type");
-                String poster = jsonArray.getJSONObject(i).getString("Poster");
-
-                // System.out.println(title + " (" + year + ")");
-                addMovie(title, year, imdb, type, poster);
+        while (run) {
+            System.out.print("\nTitle >> ");
+            Scanner ks = new Scanner(System.in);
+            String s = ks.nextLine();
+            if (s.equals("exit")) {
+                run = false;
+                System.out.println("Bye!");
+            } else {
+                getData(s);
             }
-
-            // Sortieren nach Jahr
-            Collections.sort(movies, Movie.sortByYear);
-
-            // Ausgabe Resultate
-            System.out.println("\nResults:");
-
-            for (Movie m : movies) {
-                System.out.println(m.title + " (" + m.year + ") >> " + m.type);
-            }
-
-        } else {
-            System.out.println("Unknown Status");
         }
 
 
     } // main
 
-    public static void addMovie(String title, String year, String imdb, String type, String poster) {
+    static void addMovie(String title, String year, String imdb, String type, String poster) {
         movies.add(new Movie(title, year, imdb, type, poster));
     }
+
+    static void getData(String s) throws IOException, UnknownHostException {
+        try {
+            // Request to Server
+            String url = "http://www.omdbapi.com/?apikey=58ed8f90";
+            url += "&s=" + s;
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            String responseStr = response.body().string();
+
+            // handle JSON
+            JSONObject jsonObj = new JSONObject(responseStr); // JSON Objekt erstellen
+            String responseServer = jsonObj.getString("Response");
+
+            if (responseServer.equals("False")) {
+                String errorRequest = jsonObj.getString("Error");
+                System.out.println("Fehler: " + errorRequest);
+            } else if (responseServer.equals("True")) {
+                movies.clear(); // Array leeren
+                JSONArray jsonArray = jsonObj.getJSONArray("Search"); // Objekt in Array
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String title = jsonArray.getJSONObject(i).getString("Title");
+                    String year = jsonArray.getJSONObject(i).getString("Year");
+                    String imdb = jsonArray.getJSONObject(i).getString("imdbID");
+                    String type = jsonArray.getJSONObject(i).getString("Type");
+                    String poster = jsonArray.getJSONObject(i).getString("Poster");
+                    addMovie(title, year, imdb, type, poster);
+                }
+
+                // Sortieren nach Jahr
+                Collections.sort(movies, Movie.sortByYear);
+
+                // Ausgabe Resultate
+                System.out.println("\nResults:");
+
+                for (Movie m : movies) {
+                    System.out.println(m.title + " (" + m.year + ") [" + m.type + "]");
+                }
+
+            } else {
+                System.out.println("Unknown Status");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+    } // getData()
 
 }
